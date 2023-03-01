e3db2e96c328c4740557d95fc9593f8cefa46c92
lissavzx <114089485+lissavzx@users.noreply.github.com>
2023-03-01 19:02:05 +0200

    Create ModulesKrasnitska.h

---
+++ lab08/prj/ModulesKrasnitska.h
@@ -0,0 +1,8 @@
+#ifndef MODULESKRASNITSKA_H_INCLUDED
+#define MODULESKRASNITSKA_H_INCLUDED
+
+ModulesKrasnitskaAddInt():
+
+    int ModulesKrasnitska(int i1, int i2);
+
+#endif // MODULESKRASNITSKA_H_INCLUDED
