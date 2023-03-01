23e74f735707176e59967160d9b5e9216716590b
lissavzx <114089485+lissavzx@users.noreply.github.com>
2023-03-01 19:01:45 +0200

    Create KrasnitskaTask.a.h

---
+++ lab08/prj/KrasnitskaTask/KrasnitskaTask/KrasnitskaTask.a.h
@@ -0,0 +1,8 @@
+#ifndef KRASNITSKATASK_A_H_INCLUDED
+#define KRASNITSKATASK_A_H_INCLUDED
+
+KrasnitskaTaskAddInt():
+
+    int KrasnitskaTask(int i1, int i2);
+
+#endif // KRASNITSKATASK_A_H_INCLUDED
